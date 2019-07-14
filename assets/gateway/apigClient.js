ec86cf4347dbeada658301b644c026c02b70bd74
unknown <aj2429@nyu.edu>
2019-07-14 16:42:54 -0400

    Update URL

--- assets/gateway/apigClient.js
+++ assets/gateway/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://ivp17enuc8.execute-api.us-east-1.amazonaws.com/First';
+    var invokeUrl = '';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
